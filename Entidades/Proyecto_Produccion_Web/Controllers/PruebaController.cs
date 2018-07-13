6fe1a9fa999cbbde08df82071ece2b9fa2b53769
guevarasolano <guevarasolano@hotmail.com>
2018-07-13 15:25:07 -0600

    Creación de capa web
    Creación de capa web

---
+++ Entidades/Proyecto_Produccion_Web/Controllers/PruebaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto_Produccion_Web.Controllers
+{
+    public class PruebaController : Controller
+    {
+        // GET: Prueba
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult vista()
+        {
+            return View();
+        }
+    }
+}
---
+++ Entidades/Proyecto_Produccion_Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proyecto_Produccion_Web.MvcApplication" Language="C#" %>
