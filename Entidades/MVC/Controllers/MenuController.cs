3f11226ca45a2099d170cd41376c980ff1a0c2c7
guevarasolano <guevarasolano@hotmail.com>
2018-08-24 15:59:27 -0600

    More changes

---
+++ Entidades/MVC/Controllers/MenuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers{
+
+    public class MenuController : Controller{
+
+        public ActionResult Menu(){
+            return View("~/Views/Menu/Menu.cshtml");
+        }
+
+
+    }
+}
